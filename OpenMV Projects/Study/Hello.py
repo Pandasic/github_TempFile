19c34dbc3109c66fde064e7994d26e83f725ad8a
TB521 <940706604@qq.com>
2019-05-28 19:27:20 +0800

    add

---
+++ OpenMV Projects/Study/Hello.py
@@ -0,0 +1,63 @@
+# AprilTags Example
+#
+# This example shows the power of the OpenMV Cam to detect April Tags
+# on the OpenMV Cam M7. The M4 versions cannot detect April Tags.
+
+import sensor, image, time, math
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QQVGA) # we run out of memory if the resolution is much bigger...
+sensor.skip_frames(time = 2000)
+sensor.set_auto_gain(False)  # must turn this off to prevent image washout...
+                             # 必须关闭自动增益
+sensor.set_auto_whitebal(False)  # must turn this off to prevent image washout...
+                             # 必须关闭自动白平衡
+clock = time.clock()
+
+# Note! Unlike find_qrcodes the find_apriltags method does not need lens correction on the image to work.
+# 注意, 不像寻找二维码(find_qrcodes)的例程那样, 寻找AprilTag方法, 不需要进行镜头校正(lens correlation)
+
+
+# The apriltag code supports up to 6 tag families which can be processed at the same time.
+# Returned tag objects will have their tag family and id within the tag family.
+# OpenMV AprilTag识别函数, 支持同时识别6种Family家族的Tag.
+# 返回对象信息包含Tag Family的名称, Tag ID
+tag_families = 0
+# 通过或位运算, 来决定是否识别某一种Family的Tag, 可以把不需要的注释掉
+tag_families |= image.TAG16H5 # comment out to disable this family
+tag_families |= image.TAG25H7 # comment out to disable this family
+tag_families |= image.TAG25H9 # comment out to disable this family
+tag_families |= image.TAG36H10 # comment out to disable this family
+tag_families |= image.TAG36H11 # comment out to disable this family (default family)
+tag_families |= image.ARTOOLKIT # comment out to disable this family
+
+# What's the difference between tag families? Well, for example, the TAG16H5 family is effectively
+# a 4x4 square tag. So, this means it can be seen at a longer distance than a TAG36H11 tag which
+# is a 6x6 square tag. However, the lower H value (H5 versus H11) means that the false positve
+# rate for the 4x4 tag is much, much, much, higher than the 6x6 tag. So, unless you have a
+# reason to use the other tags families just use TAG36H11 which is the default family.
+
+def family_name(tag):
+    if(tag.family() == image.TAG16H5):
+        return "TAG16H5"
+    if(tag.family() == image.TAG25H7):
+        return "TAG25H7"
+    if(tag.family() == image.TAG25H9):
+        return "TAG25H9"
+    if(tag.family() == image.TAG36H10):
+        return "TAG36H10"
+    if(tag.family() == image.TAG36H11):
+        return "TAG36H11"
+    if(tag.family() == image.ARTOOLKIT):
+        return "ARTOOLKIT"
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    for tag in img.find_apriltags(families=tag_families): # defaults to TAG36H11 without "families".
+        img.draw_rectangle(tag.rect(), color = (255, 0, 0))
+        img.draw_cross(tag.cx(), tag.cy(), color = (0, 255, 0))
+        print_args = (family_name(tag), tag.id(), (180 * tag.rotation()) / math.pi)
+        print("Tag Family %s, Tag ID %d, rotation %f (degrees)" % print_args)
+    print(clock.fps())
